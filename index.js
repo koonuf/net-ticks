e55dbc3e84753a2e507ab969f0c0f7b911c97ccd
Konstantin Onufrijev <koonuf@gmail.com>
2014-11-20 16:11:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿var TICKS_IN_MILLISECONDS = 10000,
+    MILLISECONDS_IN_HOUR = 60 * 60 * 1000,
+    NET_START_DATE = new Date("0001-01-01").getTime(),
+    MAX_TICKS = 3155378975999999999,
+    MIN_TICKS = 0;
+
+exports.netTicksToDate = function (ticks, timezoneOffsetHours) {
+    var netMilliseconds, timezoneOffsetMilliseconds;
+    
+    if (!ticks || typeof ticks !== 'number' || ticks < MIN_TICKS || ticks > MAX_TICKS) { 
+        throw 'ticks should be a number between ' + MIN_TICKS + ' and ' + MAX_TICKS;
+    }
+
+    if (timezoneOffsetHours) {
+        if (typeof timezoneOffsetHours !== 'number' || timezoneOffsetHours < -12 || timezoneOffsetHours > 14) {
+            throw 'timezoneOffsetHours is optional number between -12 and 14';
+        }
+    }
+    
+    netMilliseconds = Math.floor(ticks / TICKS_IN_MILLISECONDS);
+    timezoneOffsetMilliseconds = (timezoneOffsetHours || 0) * MILLISECONDS_IN_HOUR;
+    
+    return new Date(netMilliseconds + NET_START_DATE - timezoneOffsetMilliseconds);
+};
---
+++ tests/tests.js
@@ -0,0 +1,55 @@
+﻿var assert = require('assert');
+var ticksConverter = require('./../index.js');
+var expect = require('expect.js');
+
+var checks = [
+    {
+        ticks: 635520799092948133,
+        offsetHours: 5,
+        date: '2014-11-20 11:31:49.294 +0500'
+    },
+    {
+        ticks: 630636984690000000,
+        offsetHours: 0,
+        date: '1999-05-30 22:01:09.000 +0000'
+    },
+    {
+        ticks: 630636984690000000,
+        offsetHours: -9,
+        date: '1999-05-30 22:01:09.000 -0900'
+    }
+];
+
+describe('Checking edge values', function () {
+    it('throw error on ivalid ticks', function () {
+        var invalidConversion = function () { 
+            ticksConverter.netTicksToDate("test");
+        };
+
+        expect(invalidConversion).to.throwException();
+    });
+
+    it('throw error on ivalid timezoneOffsetHours', function () {
+        var invalidConversion = function () {
+            ticksConverter.netTicksToDate(630636984690000000, 100);
+        };
+        
+        expect(invalidConversion).to.throwException();
+    });
+    
+});
+
+describe('Checking valid ticks', function () {
+    var check;
+    for (var i = 0; i < checks.length; i++) {
+        check = checks[i];
+
+        it('should produce date ' + check.date + ' for ticks ' + check.ticks + ' with offset hours ' + check.offsetHours, function () {
+            var expected = (new Date(check.date)).getTime(),
+                actual = ticksConverter.netTicksToDate(check.ticks, check.offsetHours).getTime();
+
+            expect(actual).to.eql(expected);
+        });
+    }
+    
+});
